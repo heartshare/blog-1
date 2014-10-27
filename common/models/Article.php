4b27067193691d6f240b855c019db63cf5aafc54
Mole <niefufeng@gmail.com>
2014-10-27 23:06:34 +0800

    +文章模型

---
+++ common/models/Article.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%article}}".
+ *
+ * @property integer $id 文章ID
+ * @property string $title 文章标题
+ * @property string $content 文章内容
+ * @property integer $create_time 发表时间
+ * @property integer $modify_time 修改时间
+ * @property string $type 文章类型
+ * @property string $status 文章状态
+ * @property string $excerpt 文章摘要
+ * @property integer $home_page_show 是否在首页展示
+ * @property integer $view 查看次数
+ * @property integer $order 排序
+ * @property string $slug 路由名
+ * @property string $password 查看密码
+ * @property integer $allow_comment 是否允许评论
+ * @property integer $comments_total 评论数量
+ * @property integer $user_id 作者ID
+ * @property integer $category_id 分类ID
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%article}}';
+    }
+
+    const STATUS_PUBLISH = 'publish';//正常
+    const STATUS_DRAFT = 'draft';//草稿
+    const STATUS_DELETE = 'delete';//删除
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [
+                ['content', 'title', 'create_time', 'modify_time', 'type', 'status'],
+                'required',
+                'message' => '{attribute}不能为空',
+                'on' => ['create']
+            ],
+            [
+                ['content', 'title', 'slug', 'excerpt', 'password'],
+                'string'
+            ],
+            [
+                ['create_time', 'modify_time', 'home_page_show', 'view', 'order', 'allow_comment', 'comments_total', 'user_id', 'category_id'],
+                'integer',
+                'message' => '{attribute}只能为数字',
+                'on' => ['create','modify','view','comment']
+            ],
+            [
+                ['title'],
+                'string',
+                'min' => 1,
+                'max' => 100,
+                'message' => '{attribute}长度不符合要求'
+            ],
+            [
+                ['slug', 'password'],
+                'string',
+                'max' => 255
+            ],
+            [
+                ['title', 'slug'],
+                'unique',
+                'message' => '这个{attribute}已经被使用了'
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => '文章ID',
+            'title' => '标题',
+            'content' => '正文',
+            'create_time' => '发布时间',
+            'modify_time' => '更新时间',
+            'type' => '类型',
+            'status' => '状态',
+            'excerpt' => '文章摘要',
+            'home_page_show' => '是否推荐到首页',
+            'view' => '浏览量',
+            'order' => '排序',
+            'slug' => '路由',
+            'password' => '查看密码',
+            'allow_comment' => '是否允许评论',
+            'comments_total' => '评论统计',
+            'user_id' => '作者ID',
+            'category_id' => '分类ID',
+        ];
+    }
+
+    public function scenarios()
+    {
+        return [
+            'create' => [
+                'title',
+                'content',
+                'create_time',
+                'modify_time',
+                'type',
+                'status',
+                'excerpt',
+                'home_page_show',
+                'view',
+                'order',
+                'slug',
+                'allow_comment',
+                'comments_total',
+                'user_id',
+                'category_id'
+            ],
+            'modify' => [
+                'modify_time',
+                'title',
+                'content',
+                'status',
+                'type',
+                'order',
+                'slug'
+            ],
+            'find' => [],
+            'view' => [
+                'view',
+            ],
+            'comment' => [
+                'comments_total'
+            ]
+        ];
+    }
+
+    public function addArticle(){}
+
+    /**
+     * 获取所有的文章列表
+     * @param integer $page 页码
+     * @param integer $pageSize 页面大小
+     * @return array|\yii\db\ActiveRecord[]
+     */
+    public static function getArticleList($page,$pageSize){
+        return static::find()->indexBy('create_time')->limit($pageSize)->offset($page)->asArray()->all();
+    }
+}
