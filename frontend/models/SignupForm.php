1314113909ddf1d3e045ec19550bcfb607169feb
Mole <niefufeng@gmail.com>
2014-10-27 23:02:31 +0800

    注册表单模型完善

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -13,6 +13,10 @@ class SignupForm extends Model
     public $username;
     public $email;
     public $password;
+    public $re_password;//确认密码
+    public $create_time;
+    public $modify_time;
+    public $active_time;
 
     /**
      * @inheritdoc
@@ -20,18 +24,24 @@ class SignupForm extends Model
     public function rules()
     {
         return [
-            ['username', 'filter', 'filter' => 'trim'],
-            ['username', 'required'],
-            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
-            ['username', 'string', 'min' => 2, 'max' => 255],
+            [['username','email'], 'filter', 'filter' => 'trim'],
+            [['username','email','password'], 'required','message'=>'{attribute}不能为空'],
+            [['username','email'], 'unique', 'targetClass' => '\common\models\User', 'message' => '这个{attribute}已经被使用了'],
+            ['username', 'string', 'min' => 2, 'max' => 16,'message'=>'{attribute}在6~16位之间'],
+            [['create_time','modify_time','active_time'],'default','value'=>time()],
+            ['email', 'email','message'=>'{attribute}格式错误'],
+            [['password','re_password'], 'string', 'min' => 6,'max'=>18 ,'message'=>'{attribute}必须在6~18位之间'],
+            ['re_password','compare','compareAttribute'=>'password','message'=>'两次密码不一致'],
+        ];
+    }
 
-            ['email', 'filter', 'filter' => 'trim'],
-            ['email', 'required'],
-            ['email', 'email'],
-            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
-
-            ['password', 'required'],
-            ['password', 'string', 'min' => 6],
+    public function attributeLabels()
+    {
+        return [
+            'username' => '用户名',
+            'email' => '邮箱',
+            'password' => '密码',
+            're_password' =>'确认密码'
         ];
     }
 
@@ -44,10 +54,14 @@ class SignupForm extends Model
     {
         if ($this->validate()) {
             $user = new User();
-            $user->username = $this->username;
-            $user->email = $this->email;
+            $user->scenario = 'sign';
+            $user->attributes = $this->getAttributes();
+            $user->nickname = $this->username;
             $user->setPassword($this->password);
+            $user->generatePasswordResetToken();
             $user->generateAuthKey();
+            $user->status = $user::STATUS_ACTIVE;
+            $user->gender = $user::GENDER_DEFAULT;
             $user->save();
             return $user;
         }
