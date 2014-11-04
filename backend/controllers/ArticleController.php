09d5b28257ae6958425c9e88ff003dc895205800
Mole <niefufeng@gmail.com>
2014-11-04 08:30:48 +0800

    完善了文章删除、更新、创建和展示

--- backend/controllers/ArticleController.php
+++ backend/controllers/ArticleController.php
@@ -1,77 +1,137 @@
 <?php
+
 namespace backend\controllers;
 
-use common\models\Article;
+use common\models\Category;
 use Yii;
-use yii\filters\AccessControl;
-use yii\web\Controller;
+use yii\helpers\ArrayHelper;
+use common\models\Article;
+use common\models\ArticleSearch;
+use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
 /**
- * Site controller
+ * ArticleController implements the CRUD actions for Article model.
  */
 class ArticleController extends CommonController
 {
-    /**
-     * @inheritdoc
-     */
     public function behaviors()
     {
+        parent::behaviors();
         return [
-//            'access' => [
-//                'class' => AccessControl::className(),
-//                'rules' => [
-//                    [
-//                        'actions' => ['login', 'error'],
-//                        'allow' => true,
-//                    ],
-//                    [
-//                        'actions' => ['logout', 'index'],
-//                        'allow' => true,
-//                        'roles' => ['@'],
-//                    ],
-//                ],
-//            ],
-//            'verbs' => [
-//                'class' => VerbFilter::className(),
-//                'actions' => [
-//                    'logout' => ['post'],
-//                ],
-//            ],
-        ];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
             ],
         ];
     }
 
     /**
-     * 文章列表
-     * @return string
+     * Lists all Article models.
+     * @return mixed
      */
-    public function actionList()
+    public function actionIndex()
     {
-        $list = Article::getArticleList(
-            Yii::$app->request->get('page',1),
-            Yii::$app->request->get('pageSize',20)
-        );
-        return $this->render('list',['list'=>$list]);
+        $searchModel = new ArticleSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
     }
 
-    public function actionAdd(){
+    /**
+     * Displays a single Article model.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Article model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
         $model = new Article();
         $model->scenario = 'create';
-        if ($model->load(Yii::$app->request->post()) && $model->addArticle()){
-
+        if ($model->load(Yii::$app->request->post())) {
+            if (!$model->save()) {
+                Yii::$app->session->setFlash('error','验证或保存数据失败！');
+            }else {
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
         }
-        return $this->render('add',['model'=>$model]);
+        //设置默认值
+        $model->type = 'post';
+        $model->status = $model::STATUS_PUBLISH;
+        $model->home_page_show = 0;
+        $model->allow_comment = 1;
+        //获取分类
+        $categorys = Category::findAll(['parent_id' => 0]);
+        $categorys = ArrayHelper::map($categorys,'id','category_name');
+        return $this->render('create', [
+            'model' => $model,
+            'categorys' => $categorys
+        ]);
+    }
+
+    /**
+     * Updates an existing Article model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+        $model->scenario = 'modify';
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            $categorys = Category::findAll(['parent_id' => 0]);
+            $categorys = ArrayHelper::map($categorys,'id','category_name');
+            return $this->render('update', [
+                'model' => $model,
+                'categorys' => $categorys
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Article model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Article model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $id
+     * @return Article the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Article::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
     }
 }
